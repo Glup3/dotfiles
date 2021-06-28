64423799a45e996b87c8a381be5be680e8570d14
Phuc Tran <glup3.tran@gmail.com>
2021-06-28 16:35:10 +0200

    add skhd

---
+++ .config/skhd/scripts/moveWindowLeftAndFollowFocus.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curWindowId="$(yabai -m query --windows --window | jq -re ".id")"
+
+$(yabai -m window --display prev || yabai -m window --display last)
+$(yabai -m window --focus "$curWindowId")
---
+++ .config/skhd/scripts/moveWindowPrevSpace.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curWindowId="$(yabai -m query --windows --window | jq -re ".id")"
+
+$(yabai -m window --space prev || yabai -m window --space last)
+$(yabai -m window --focus "$curWindowId")
