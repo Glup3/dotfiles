be0f699d6265db130c7575b102e5e438e4b52848
Phuc Tran <glup3.tran@gmail.com>
2022-10-09 21:52:18 +0200

    add karabiner

--- dragonite/symlinks.sh
+++ dragonite/symlinks.sh
@@ -4,3 +4,4 @@ BASEPATH=$( dirname -- "$( readlink -f -- "$0"; )"; )
 
 ln -sfn $BASEPATH/.config/skhd ~/.config/skhd
 ln -sfn $BASEPATH/.config/yabai ~/.config/yabai
+ln -sfn $BASEPATH/.config/karabiner ~/.config/karabiner
